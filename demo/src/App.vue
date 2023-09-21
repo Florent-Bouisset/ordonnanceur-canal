7eb343d756d09bb8f410291b1f8d0065346edba2
Florent Bouisset <florent.bouisset@jellysmack.com>
2023-09-21 15:26:36 +0200

    create a demo

---
+++ demo/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import RequestComponent from "./components/RequestComponent.vue";
+import { RequestScheduler } from "../../src/lib/main";
+const scheduler = new RequestScheduler();
+</script>
+
+<template>
+  <div>
+    <table>
+      <thead>
+        <td>Priority</td>
+        <td>Status</td>
+      </thead>
+      <tbody>
+        <RequestComponent :priority="3" status="in progress"></RequestComponent>
+        <RequestComponent :priority="2" status="waiting"></RequestComponent>
+        <RequestComponent :priority="4" status="done"></RequestComponent>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style>
+table,
+th,
+td {
+  border: 1px solid white;
+}
+
+table {
+  border-collapse: collapse;
+}
+
+td {
+  padding: 10px 20px;
+}
+</style>
---
+++ demo/src/components/RequestComponent.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{ priority: number; status: string }>();
+
+const getColor = computed(() => {
+  switch (props.priority) {
+    case 1:
+      return "green";
+
+    case 2:
+      return "yellow";
+
+    case 3:
+      return "orange";
+
+    case 4:
+      return "red";
+
+    default:
+      return "grey";
+  }
+});
+</script>
+
+<template>
+  <tr>
+    <td :style="`color: ${getColor}`">{{ priority }}</td>
+    <td>{{ status }}</td>
+  </tr>
+</template>
