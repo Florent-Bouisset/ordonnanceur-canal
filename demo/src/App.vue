8148946ecdd4258f82199dd56da99f8dcf6e7b39
Florent Bouisset <florent.bouisset@jellysmack.com>
2023-09-21 16:00:51 +0200

    make display reactive

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,29 +1,67 @@
 <script setup lang="ts">
 import RequestComponent from "./components/RequestComponent.vue";
 import { RequestScheduler } from "../../src/lib/main";
-const scheduler = new RequestScheduler();
+import { computed, ref } from "vue";
+const scheduler = ref(new RequestScheduler());
 
-scheduler.addRequest({
+scheduler.value.addRequest({
   priority: 1,
   url: "https://rickandmortyapi.com/api/character/1",
 });
 
-scheduler.addRequest({
+scheduler.value.addRequest({
   priority: 2,
   url: "https://rickandmortyapi.com/api/character/2",
 });
 
-scheduler.addRequest({
+scheduler.value.addRequest({
   priority: 3,
   url: "https://rickandmortyapi.com/api/character/3",
 });
 
-const requestArray = Array.from(scheduler.requestMap.values());
-const nextRequests = scheduler.getRequestsToExecute();
+scheduler.value.addRequest({
+  priority: 3,
+  url: "https://rickandmortyapi.com/api/character/4",
+});
+
+scheduler.value.addRequest({
+  priority: 4,
+  url: "https://rickandmortyapi.com/api/character/5",
+});
+
+const requestArray = computed(() =>
+  Array.from(scheduler.value.requestMap.values())
+);
+const nextRequests = computed(() => scheduler.value.getRequestsToExecute());
+
+const priority = ref(1);
+const handlePriorityChange = (e: any) => {
+  priority.value = Number.parseInt(e.target.selectedOptions[0].value);
+};
+
+const addRequestHandler = () => {
+  scheduler.value.addRequest({
+    priority: priority.value,
+    url: "https://rickandmortyapi.com/api/character/3",
+  });
+};
 </script>
 
 <template>
   <div>
+    <h2>Create a new request</h2>
+    <label for="priority">Priority</label>
+
+    <select id="priority" :onchange="handlePriorityChange">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+    </select>
+
+    <button style="margin-left: 24px" :onclick="addRequestHandler">
+      Add request
+    </button>
     <h2>All Requests</h2>
     <table>
       <thead>
